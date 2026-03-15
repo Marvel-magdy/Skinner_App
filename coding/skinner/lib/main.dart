79034d29e0ac6ceb25c8d04431fa8ec75a0d633c
Marvel-magdy <marvelmagde@gmail.com>
2026-03-15 05:27:39 +0200

    home_screen

---
+++ coding/skinner/lib/main.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(const Skinner());
+
+}
+class Skinner extends StatelessWidget {
+  const Skinner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Splashscreen(),
+    );
+  }
+}
